d9351f6b1e2e5c7a1b4d33c994de3e39fd57bb71
Leonardo Lama <leolama18@gmail.com>
2019-02-07 01:58:06 -0400

    I like big buttsd
    and i must not lie

---
+++ platzigram/platzigram/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from datetime import datetime
+import pdb
+import json
+
+def hello_world(request):
+
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')))
+
+
+def sort_integers(request):
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': "Integers sorted successfully."
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+
+def say_hi(request, name, age):
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello {}: Welcome to Platzigrame'.format(name)
+    return HttpResponse(message)
---
+++ platzigram/posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from datetime import datetime
+
+posts = [
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yesica Cortes',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600/?image=1036',
+    },
+    {
+        'title': 'Via Lactea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo Auditorio',
+        'user': {
+            'name': 'Uriel',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/200/200/?image=1076',
+    }
+
+
+]
+
+def list_posts(request):
+    return render(request, 'feed.html', {'posts': posts})
