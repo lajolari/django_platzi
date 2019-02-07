e08bd15198839ca8bc66d6734d7134415267ec0c
Leonardo <leolama18@gmail.com>
2019-02-07 14:45:24 -0400

    commit and push

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
 
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
